779bcc493b48d4429e4636a72bc9cf7fef913f67
Kelson <shquapp@gmail.com>
2023-07-31 21:46:33 -0700

    Creating separate outlined version of interactable objects

--- Assets/Scripts/MouseOver.cs
+++ Assets/Scripts/MouseOver.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+//The functions below will execute on whatever object they are attached to. In order for them to be detected the object
+//needs to have a collider and that collider has to be part of the event system.
+
 public class MouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
-    // Start is called before the first frame update
+    //Triggers when mouse hovers over attached object
     public void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("Mouse is over the Ship");
     }
 
+    //Triggers when mouse is no longer hovering over attached object
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log("Mouse is NOT over the Ship");
     }
 
+    //Triggers when mplayer clicks on attached object
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("The ship has been clicked on");
